7739746ec91aac474d44df3c188ba8242674478c
cuongdinh1507 <cuongdz1507@gmail.com>
2019-09-03 22:05:13 +0700

    up

--- resources/views/addNew.blade.php
+++ resources/views/addNew.blade.php
@@ -94,9 +94,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                                <button type="submit" class="btn btn-primary">Next</button>
                             </div>
                         </div>
                     </form>
