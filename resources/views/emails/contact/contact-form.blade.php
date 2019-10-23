cfd3724fb885822b085050e01ae35bd7fa5daf96
Cường Đinh <35897209+cuongdinh1507@users.noreply.github.com>
2019-10-23 15:31:14 +0700

    u00p

--- resources/views/emails/contact/contact-form.blade.php
+++ resources/views/emails/contact/contact-form.blade.php
@@ -3,7 +3,7 @@
 <div>From: <strong>{{ $data['email'] }}</strong></div>
 <div>Name: <strong>{{ $data['name'] }}</strong></div>
 <div>Organization: <strong>{{ $data['organization'] }}</strong></div>
-<div>Field of interset: <strong>{{ $data['email'] }}</strong></div>
+<div>Field of interset: <strong>{{ $data['field'] }}</strong></div>
 <div>Message:</div>
 <div>{{$data['message']}}</div>
 
